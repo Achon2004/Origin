f8c673c698392514cf2a0084c0d89711aa299a43
Anderson <andersonastudillo2004@gmail.com>
2025-06-18 13:57:15 -0500

    Se agregó la función de elevar al cubo

--- calculadora_git.py
+++ calculadora_git.py
@@ -16,9 +16,13 @@ def dividir(a: float, b: float) -> float:
 def potencia(a: float, b: float) -> float:
     return a ** b
 
+def elevar_cubo(a: float) -> float:
+    return a ** 3
+
 if __name__ == "__main__":
     print(sumar(2,3))
     print(restar(5,1))
     print(multiplicar(4,2))
     print(dividir(10,2))
     print(potencia(2,3))
+    print(elevar_cubo(5))
